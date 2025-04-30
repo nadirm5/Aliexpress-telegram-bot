5a7c6e619310eab748cc3a779bb088bf10728b60
nadirm5 <nadir.med55555@gmail.com>
2025-04-30 13:59:21 +0000

    Update app.py

--- app.py
+++ app.py
@@ -61,7 +61,7 @@ SHORT_LINK_DOMAIN_REGEX = re.compile(r'https?://(?:s\.click\.aliexpress\.com/e/|
 COMBINED_DOMAIN_REGEX = re.compile(r'aliexpress\.com|s\.click\.aliexpress\.com|a\.aliexpress\.com', re.IGNORECASE)
 
 OFFER_PARAMS = {
-    "coin": {"name": "🪙 **Coins**: ⭐ **اضغط هنا على الرابط لتجد أقل سعر تخفيض بالعملات!** 💥👉", "params": {"sourceType": "620%26channel=coin", "afSmartRedirect": "y"}}
+    "coin": {"name": "🪙 **Coins**: ⭐ **اضغط هنا على الرابط لتجد أقل سعر تخفيض بالعملات!** 💥👉", "params": {"sourceType": "620%26channel=coin", "afSmartRedirect": "y"}},
     
     "super": {"name": "🔥 Super Deals", "params": {"sourceType": "562", "channel": "sd", "afSmartRedirect": "y"}},
     "limited": {"name": "⏳ Limited Offers", "params": {"sourceType": "561", "channel": "limitedoffers", "afSmartRedirect": "y"}},
