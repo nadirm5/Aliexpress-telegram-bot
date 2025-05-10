6644ab5331dbfd7e364d51c7a6ef3d3d9d7e0940
nadirm5 <nadir.med55555@gmail.com>
2025-05-10 14:22:35 +0100

    Update app.py

--- app.py
+++ app.py
@@ -535,6 +535,13 @@ def _build_response_message(product_data: dict, generated_links: dict, details_s
     if details_source == "API" and product_price:
         price_str = f"{product_price} {product_currency}".strip()
         message_lines.append(f"\n💰 <b>Price السعر بدون تخفيض:</b> {price_str}\n")
+        "coin": {
+        "name": "🪙 <b>🎯 Coins</b> – <b>  ⬇️ السعر بالتخفيص  💸</b> 👉",
+        "params": {
+            "sourceType": "620%26channel=coin",
+            "afSmartRedirect": "y"
+        }
+    },
     elif details_source == "Scraped":
         message_lines.append("\n💰 <b>Price:</b> Unavailable (Scraped)\n")
     else:
