d8f3596d9b922050dad8a9209e22b3ba04724326
nadirm5 <nadir.med55555@gmail.com>
2025-05-11 14:21:48 +0100

    Update app.py

--- app.py
+++ app.py
@@ -57,21 +57,24 @@ except Exception as e:
 
 
 
-product_data = products[0]
-
 try:
+    product_data = products[0]  # Assurez-vous que products[0] est défini correctement
     offer_data = await get_offer_price(product_id, ALIEXPRESS_TRACKING_ID)
+
     sale_price = offer_data.get('sale_price', 'N/A')
     original_price = offer_data.get('original_price', product_data.get('target_sale_price'))
     savings = offer_data.get('savings', '0')
+
 except Exception as e:
     print("Erreur lors de la récupération du prix coins:", e)
+    # Si une erreur survient, définir les valeurs par défaut
+    product_data = {}
     sale_price = 'N/A'
-    original_price = product_data.get('target_sale_price')
+    original_price = 'N/A'
     savings = '0'
 
 product_info = {
-    'image_url': product_data.get('product_main_image_url'),
+    'image_url': product_data.get('product_main_image_url', ''),
     'price': original_price,
     'currency': product_data.get('target_sale_price_currency', TARGET_CURRENCY),
     'title': product_data.get('product_title', f'Product {product_id}'),
