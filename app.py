75f4f351d1d4587e5f5dcecbba48db6142ec043e
nadirm5 <nadir.med55555@gmail.com>
2025-05-17 08:50:00 +0000

    Update app.py

--- app.py
+++ app.py
@@ -62,55 +62,26 @@ STANDARD_ALIEXPRESS_DOMAIN_REGEX = re.compile(r'https?://(?!a\.|s\.click\.)([\w-
 SHORT_LINK_DOMAIN_REGEX = re.compile(r'https?://(?:s\.click\.aliexpress\.com/e/|a\.aliexpress\.com/_)[a-zA-Z0-9_-]+/?', re.IGNORECASE)
 COMBINED_DOMAIN_REGEX = re.compile(r'aliexpress\.com|s\.click\.aliexpress\.com|a\.aliexpress\.com', re.IGNORECASE)
 
-OFFER_PARAMS = { 
-  "coin": { 
-    "name": "🪙 <b>🎯 Coins</b> – <b>الرابط بالتخفيض ⬇️ أقل سعر بالعملات 💸</b> 👉", 
-    "params": { 
-      "sourceType": "620%26channel=coin", 
-      "afSmartRedirect": "y" 
-    } 
-  }, 
-  "link": { 
-    "name": "🚀 <b>🔗 رابط المنتوج بالتخفيض</b>", 
-    "params": { 
-      "sourceType": "620%26channel=coin", 
-      "afSmartRedirect": "y" 
-    } 
-  }, 
-  "super": { 
-    "name": "🔥 Super Deals", 
-    "params": { 
-      "sourceType": "562", 
-      "channel": "sd", 
-      "afSmartRedirect": "y" 
-    } 
-  }, 
-  "limited": { 
-    "name": "⏳ Limited Offers", 
-    "params": { 
-      "sourceType": "561", 
-      "channel": "limitedoffers", 
-      "afSmartRedirect": "y" 
-    } 
-  }, 
-  "bigsave": { 
-    "name": "💰 Big Save", 
-    "params": { 
-      "sourceType": "680", 
-      "channel": "bigSave", 
-      "afSmartRedirect": "y" 
-    } 
-  }, 
-  "bundle": { 
-    "name": "📦 <b>Bundle Deals – عروض التجميع</b> ⬇️ 👉", 
-    "params": { 
-      "sourceType": "681", 
-      "channel": "bundledeals", 
-      "afSmartRedirect": "y" 
-    } 
-  } 
+OFFER_PARAMS = {
+    "coin": {
+        "name": "🪙 <b>🎯 Coins</b> – <b>الرابط بالتخفيض ⬇️ أقل سعر بالعملات 💸</b> 👉",
+        "params": {
+            "sourceType": "620%26channel=coin",
+            "afSmartRedirect": "y"
+        }
+    },
+    "bundle": {
+        "name": "📦 <b>Bundle Deals</b> – <b>أفضل تخفيض من عروض البندل</b> 🔗",
+        "params": {
+            "sourceType": "690",
+            "channel": "bundle",
+            "afSmartRedirect": "y"
+        }
+    }
 }
-OFFER_ORDER = ["coin", "link", "super", "limited", "bigsave", "bundle"]
+
+OFFER_ORDER = ["coin", "bundle"]
+
 class CacheWithExpiry:
     def __init__(self, expiry_seconds):
         self.cache = {}
