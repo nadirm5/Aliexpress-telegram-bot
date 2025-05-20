d2382105bf5c1041a3989b00a72ebf9ea05c21aa
nadirm5 <nadir.med55555@gmail.com>
2025-05-20 08:31:08 +0100

    Update app.py

--- app.py
+++ app.py
@@ -308,7 +308,7 @@ async def fetch_product_details_v2(product_id: str) -> dict | None:
         product_data = products[0]
         product_info = {
             'image_url': product_data.get('product_main_image_url'),
-             'price': int(product_data.get('BEFORE_PAYMENT_PROMOTION_ORDER_PAYCAS_CENT', 0)) / 100,), 
+             'price': int(product_data.get('BEFORE_PAYMENT_PROMOTION_ORDER_PAYCAS_CENT', 0)) / 100'), 
             'currency': product_data.get('target_sale_price_currency', TARGET_CURRENCY),
             'title': product_data.get('product_title', f'Product {product_id}')
         }
