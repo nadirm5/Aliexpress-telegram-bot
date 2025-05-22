6c1a68ad2d2dafe9363645ab0676213ebcc6ec0a
nadirm5 <nadir.med55555@gmail.com>
2025-05-22 12:26:53 +0100

    Update app.py

--- app.py
+++ app.py
@@ -72,7 +72,6 @@ OFFER_PARAMS = {
             "scm_id": "1007.54490.406276.0",
             "scm-url": "1007.54490.406276.0",
             "pvid": "22d498ad-f0e6-45c1-9230-67ba6f431490",
-            }
         }
     },
     "bundle": {
@@ -87,6 +86,7 @@ OFFER_PARAMS = {
         }
     }
 }
+
 OFFER_ORDER = ["coin", "bundle"]
 class CacheWithExpiry:
     def __init__(self, expiry_seconds):
