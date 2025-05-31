4f3a773295ca9761f714e69805b0780503dd103e
nadirm5 <nadir.med55555@gmail.com>
2025-05-31 11:34:49 +0100

    Update aliexpress_utils.py

--- aliexpress_utils.py
+++ aliexpress_utils.py
@@ -94,7 +94,7 @@ def get_product_details_by_id(product_id):
     Returns:
         tuple: (product_name, img_url) or (None, None) if failed.
     """
-    product_url = "aliexpress://coinsHome"
+    product_url = f"https://www.aliexpress.com/item/{product_id}.html"
     print(f"Constructed URL: {product_url}")
     return get_aliexpress_product_info(product_url)
     
