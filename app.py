ba1bc166c74210eb3122710843cfa15d48b48814
nadirm5 <nadir.med55555@gmail.com>
2025-06-01 10:41:45 +0100

    Update app.py

--- app.py
+++ app.py
@@ -580,15 +580,15 @@ def _build_response_message(product_data: dict, generated_links: dict, details_s
 def _build_reply_markup() -> InlineKeyboardMarkup:
     keyboard = [
         [
-            InlineKeyboardButton("🎫 كوبونات  |  Coupons", url="https://s.click.aliexpress.com/e/_oliYXEJ"),
-            InlineKeyboardButton("🔥 Deal of the Day", url="https://s.click.aliexpress.com/e/_omRiewZ")
+            InlineKeyboardButton("🎫 Coupons", url="https://s.click.aliexpress.com/e/_oliYXEJ"),
+            InlineKeyboardButton("🔥 Deal ", url="https://s.click.aliexpress.com/e/_omRiewZ")
         ],
         [
             InlineKeyboardButton("🛍️ Bundle Deals", url="https://s.click.aliexpress.com/e/_oE0GKJ9")
         ],
         [
-            InlineKeyboardButton("📢 Join VIP Channel", url="https://t.me/RayanCoupon"),
-            InlineKeyboardButton("❤️ Support Me", url="https://moneyexpress.fun")
+            InlineKeyboardButton("📢 Join Channel", url="https://t.me/RayanCoupon"),
+            InlineKeyboardButton("❤️ website", url="https://moneyexpress.fun")
         ]
     ]
     return InlineKeyboardMarkup(keyboard)
