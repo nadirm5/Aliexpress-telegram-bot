0148ff1d28f1c79135af05abfbff725c336f6436
nadirm5 <nadir.med55555@gmail.com>
2025-06-01 12:02:00 +0100

    Update app.py

--- app.py
+++ app.py
@@ -64,16 +64,17 @@ SHORT_LINK_DOMAIN_REGEX = re.compile(r'https?://(?:s\.click\.aliexpress\.com/e/|
 COMBINED_DOMAIN_REGEX = re.compile(r'aliexpress\.com|s\.click\.aliexpress\.com|a\.aliexpress\.com', re.IGNORECASE)
 OFFER_PARAMS = {
     "coin": {
-        "name": "🪙 <b>🎯 Coins</b> – <b>الرابط بالتخفيض ⬇️ أقل سعر بالعملات 💸</b> 👉",
-        "params": {
-            "sourceType": "620&channel=coin",
-            "afSmartRedirect": "y",
-            "scm": "1007.54490.406276.0",
-            "scm_id": "1007.54490.406276.0",
-            "scm-url": "1007.54490.406276.0",
-            "pvid": "9e4d75a0-fd0a-4eed-94d7-deb73bce909f"
-        }
-    },
+    "name": "🪙 <b>🎯 Coins</b> – <b>الرابط بالتخفيض ⬇️ أقل سعر بالعملات 💸</b> 👉",
+    "params": {
+        "sourceType": "620",
+        "channel": "coin",
+        "afSmartRedirect": "y",
+        "scm": "1007.54490.406276.0",
+        "scm_id": "1007.54490.406276.0",
+        "scm-url": "1007.54490.406276.0",
+        "pvid": "9e4d75a0-fd0a-4eed-94d7-deb73bce909f"
+    }
+},
     "bundle": {
         "name": "📦 <b>Bundle Deals</b> – <b>خصومات على العروض المجمعة 💥</b> 👉",
         "params": {
