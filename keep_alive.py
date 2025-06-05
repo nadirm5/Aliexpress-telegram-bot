198439d65c6891fb26be9ef415243d53964701bc
nadirm5 <nadir.med55555@gmail.com>
2025-06-05 08:42:55 +0100

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,65 +1,39 @@
 # keep_alive.py
 
 
-
-from flask import Flask
-
+from flask import Flask, request
 from threading import Thread
-
 import requests
-
 import time
-
-
 
 app = Flask(__name__)
 
-
-
 @app.route('/')
-
 def home():
-
     return "?"
 
-
+@app.route('/callback')
+def callback():
+    code = request.args.get('code')
+    if not code:
+        return "No code received", 400
+    return "Callback received with code: " + code
 
 def run():
-
     app.run(host='0.0.0.0', port=8080)
 
-
-
 def keep_alive():
-
     t = Thread(target=run)
-
     t.start()
 
-
-
 def self_ping():
-
     while True:
-
         try:
-
-            requests.get("https://aliexpress-telegram-bot-qjdv.onrender.com")  
-
+            requests.get("https://aliexpress-telegram-bot-qjdv.onrender.com")
             print(" hello world ")
-
         except:
-
             print("fail ping")
-
         time.sleep(60 * 3)
 
-		
-
-		
-
-		
-
 keep_alive()
-
-Thread(target=self_ping).start()		
+Thread(target=self_ping).start()
