e29a332893094a0a593df78685704a08743f03c6
nadirm5 <nadir.med55555@gmail.com>
2025-06-10 09:19:05 +0100

    Update app.py

--- app.py
+++ app.py
@@ -66,7 +66,7 @@ OFFER_PARAMS = {
     "coin": {
         "name": "🪙 <b>🎯 Coins</b> – <b>الرابط بالتخفيض ⬇️ أقل سعر بالعملات 💸</b> 👉",
         "params": {
-            "sourceType": "https://www.aliexpress.com/item/1005006324961162.html?sourceType=620&channel=coin&pdp_ext_f=%7B%22",
+            "sourceType": "620&channel=coin&pdp_ext_f=%7B%22",
             "afSmartRedirect": "y"
         }
     },
