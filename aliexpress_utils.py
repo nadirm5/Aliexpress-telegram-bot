f0cc035a3e173b2093df42599db45d40e9623390
nadirm5 <nadir.med55555@gmail.com>
2025-06-12 11:46:29 +0100

    Update aliexpress_utils.py

--- aliexpress_utils.py
+++ aliexpress_utils.py
@@ -15,7 +15,7 @@ def get_aliexpress_product_info(product_url):
     img_url = None # Initialize img_url
     try:
         headers = {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0"
         }
         cookies = {"x-hng": "lang=en-US", "intl_locale": "en_US"}
         response = requests.get(product_url, headers=headers, cookies=cookies, timeout=15)
